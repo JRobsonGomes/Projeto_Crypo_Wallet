a39bf744d99b38801f651edbb89e463f3cec85b1
JRobsonGomes <robsong200@gmail.com>
2020-03-15 10:15:32 -0300

    Refatorando Códigos

--- db/seeds.rb
+++ db/seeds.rb
@@ -11,20 +11,47 @@
 # comandos Rails para apagar e recriar todos os dados
 # rails db:drop db:create db:migrate db:seed
 
-Coin.create!(
-  description: 'Bitcoin',
-  acronym: 'BTC',
-  url_image: 'https://toppng.com/uploads/preview/bitcoin-png-bitcoin-logo-transparent-background-11562933997uxok6gcqjp.png'
-)
+# Coin.create!(
+#   [
+#     {
+#       description: 'Bitcoin',
+#       acronym: 'BTC',
+#       url_image: 'https://toppng.com/uploads/preview/bitcoin-png-bitcoin-logo-transparent-background-11562933997uxok6gcqjp.png'
+#     },
+#     {
+#       description: 'Ethereum',
+#       acronym: 'ETH',
+#       url_image: 'https://cdn2.iconfinder.com/data/icons/cryptocurrency-5/100/cryptocurrency_blockchain_crypto-02-512.png'
+#     },
+#     {
+#       description: 'Dash',
+#       acronym: 'DASH',
+#       url_image: 'https://guiadobitcoin.com.br/wp-content/uploads/2018/06/Dash-Logo.png'
+#     }
+#   ]
+# )
 
-Coin.create!(
-  description: 'Ethereum',
-  acronym: 'ETH',
-  url_image: 'https://cdn2.iconfinder.com/data/icons/cryptocurrency-5/100/cryptocurrency_blockchain_crypto-02-512.png'
-)
+spinner = TTY::Spinner.new('[:spinner] Cadastrando moedas...')
+spinner.auto_spin
+coins = [
+  {
+    description: 'Bitcoin',
+    acronym: 'BTC',
+    url_image: 'https://toppng.com/uploads/preview/bitcoin-png-bitcoin-logo-transparent-background-11562933997uxok6gcqjp.png'
+  },
+  {
+    description: 'Ethereum',
+    acronym: 'ETH',
+    url_image: 'https://cdn2.iconfinder.com/data/icons/cryptocurrency-5/100/cryptocurrency_blockchain_crypto-02-512.png'
+  },
+  {
+    description: 'Dash',
+    acronym: 'DASH',
+    url_image: 'https://guiadobitcoin.com.br/wp-content/uploads/2018/06/Dash-Logo.png'
+  }
+]
 
-Coin.create!(
-  description: 'Dash',
-  acronym: 'DASH',
-  url_image: 'https://guiadobitcoin.com.br/wp-content/uploads/2018/06/Dash-Logo.png'
-)
+coins.each do |coin|
+  Coin.find_or_create_by!(coin)
+end
+spinner.success('(Concluido!)')
--- lib/tasks/dev.rake
+++ lib/tasks/dev.rake
@@ -4,12 +4,24 @@ namespace :dev do
   desc 'Configura o ambiente de desenvolvimento'
   task setup: :environment do
     if Rails.env.development?
-      spinner = TTY::Spinner.new("[:spinner] Executando tarefas")
-      spinner.auto_spin
-      %x(rails db:drop db:create db:migrate db:seed)
-      spinner.success('(Concluido com sucesso!)')
+      show_spinner('Apagando BD...') {%x(rails db:drop)}
+
+      show_spinner('Criando BD...') {%x(rails db:create)}
+
+      show_spinner('Migrando BD...') {%x(rails db:migrate)}
+
+      show_spinner('Populando BD...') {%x(rails db:seed)}
     else
-      puts "Você não está em ambiente de desenvolvimento!"
+      puts 'Você não está em ambiente de desenvolvimento!'
     end
+  end
+
+  private
+
+  def show_spinner(msg_start, msg_end = 'Concluido!')
+    spinner = TTY::Spinner.new("[:spinner] #{msg_start}")
+    spinner.auto_spin
+      yield
+    spinner.success("(#{msg_end})")
   end
 end
